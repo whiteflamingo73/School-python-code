4021e40db1696d9d1e5698491d991b260380e20a
whiteflamingo73 <156980239+whiteflamingo73@users.noreply.github.com>
2024-12-21 19:46:29 -0500

    Finished version of hangman

--- Python/Unit07_Problem08_Bargdill.py
+++ Python/Unit07_Problem08_Bargdill.py
@@ -12,49 +12,89 @@ window = turtle.Screen()
 
 mysteryWord = ["*", "*", "*", "*", "*", "*"]
 
-correctAnswer = 0
-wrongAnswer = 0
+incorrectLetter = ["Incorect letters:"]
 
-def drawHangman(num, correctAnswer, wrongAnswer, ASCIIword, copiedWord):
+wrongAnswer = [0]
+
+def drawHangman(num, wrongAnswer, ASCIIword, copiedWord):
     
-    
-    
-    print(TheWord)
+    correctAnswer = 0
+    #this variable is completely useless and is just for the single if statement, I don't want to accidently break the code
+    holdingVariable = 0
+
     
     
     for i in range(6):
         if num == ASCIIword[i]:
-            print("correct")
+
             mysteryWord.pop(i)
             #need to make this part work
             #if chr(num) == mysteryWord[i]:
             #    print("You already guessed that")
-            #    correctAnswer -= 1
             mysteryWord.insert(i, chr(ASCIIword[i]))
-
             correctAnswer += 1
+
         
-        if mysteryWord == copiedWord:
-            print("you got the word!")
-            break
+       
+        
         #need to make it so if there's one correct it will ignore all the wrongs
         #this part should also draw the hangman
         else:
-            print("false")
-            wrongAnswer += 1
+            holdingVariable += 1
+
+    #counts how many wrong guesses you've had and also adds 
+    if correctAnswer == 0:
+        wrongAnswer[0] += 1
+        incorrectLetter.append(chr(num))
+        print(incorrectLetter)
+
         
+
+    if mysteryWord == copiedWord:
+        print("you got the word!")
+
+    #The wrong guesses(drawing the hangman):
+
+    if wrongAnswer[0] == 1:
+        myturtle.penup()
+        myturtle.goto(0,150)
+        myturtle.pendown()
+        myturtle.circle(50)
+
+    if wrongAnswer[0] == 2:
+        myturtle.goto(0,0)
+    
+    if wrongAnswer[0] == 3:
+        myturtle.penup()
+        myturtle.goto(0,140)
+        myturtle.pendown()
+        myturtle.goto(-50,140)
+    
+    if wrongAnswer[0] == 4:
+        myturtle.goto(50,140)
+    
+    if wrongAnswer[0] == 5:
+        myturtle.penup()
+        myturtle.goto(0,0)
+        myturtle.pendown()
+        myturtle.goto(-25,-50)
+
+    if wrongAnswer[0] >= 6:
+        print("You Lost")
+        myturtle.penup()
+        myturtle.goto(0,0)
+        myturtle.pendown()
+        myturtle.goto(25,-50)
+        myturtle.color("red")
+        myturtle.write("Game Over")
+        
+
+    print("The Mystery Word:", mysteryWord)
+
     
 
-    if wrongAnswer >= 6:
-        print("You're Wrong")
-    print(ASCIIword)
-    print(num)
-    print(mysteryWord)
-
-    #print(f'corrects: {correctAnswer}')
-    #print(f'wrongs: {wrongAnswer}')
-    #figure out how to return the wrong and correct answers
-    return(mysteryWord, wrongAnswer, correctAnswer)
+    
+    return mysteryWord, wrongAnswer
 
 
 wordbank = [
@@ -68,24 +108,47 @@ wordbank = [
     ['q','u','a','r','t','z'],
     ['z','o','m','b','i','e'],
     ['p','a','z','a','z','z'],
-    ['y','e','l','l','o','w']
+    ['y','e','l','l','o','w'],
+    ['d','a','b','b','l','e'],
+    ['f','a','b','r','i','c'],
+    ['h','a','b','i','t','s'],
+    ['r','a','b','b','i','t'],
+    ['m','a','c','h','e','s'],
+    ['m','a','c','a','w','s'],
+    ['m','a','n','g','o','s'],
+    ['p','a','c','i','f','y'],
+    ['t','a','b','l','e','s'],
+    ['u','g','l','i','f','y'],
+    ['v','a','c','u','u','m'],
+    ['v','a','c','a','t','e'],
+    ['w','a','c','k','e','d'],
+    ['y','a','c','h','t','s']
 ]
 
 
 TheWord = random.choice(wordbank)
 
+
+copiedWord = TheWord.copy()
+#wanna make it so you can have more than just 6 guesses
+
+
 ASCIIword = []
 for i in range(6):
     ASCIIword.append(ord(TheWord.pop(0)))
 
-print(TheWord)
-copiedWord = TheWord.copy()
-#wanna make it so you can have more than just 6 guesses
+
+
+print("This is hangman, all words are 6 letters, and if you get 6 guesses wrong, you lose")
 
 for i in range(100):
     guess = ord(input("What is your guess for a letter in the word?: "))
-    drawHangman(guess, correctAnswer, wrongAnswer, ASCIIword, copiedWord)
-    print(f'correct answers: {correctAnswer}, wrong answers: {wrongAnswer}')
+    drawHangman(guess, wrongAnswer, ASCIIword, copiedWord)
+    if wrongAnswer[0] == 6:
+        print(f"The word was{copiedWord}")
+        break
+    if mysteryWord == copiedWord:
+        break
 
 
         
